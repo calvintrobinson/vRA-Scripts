b04bc4afa0c345a13baba3aaeaa0a5fad4c87c8f
Calvin Robinson <calvintrobinson@gmail.com>
2015-10-08 15:00:10 -0400

    chpassword.sh

---
+++ chpassword.sh
@@ -0,0 +1,4 @@
+#CHPASSWORD SCRIPT
+/bin/grep '"vra.gugent.selfmanaged.password.linux"' /usr/share/gugent/site/InstallSoftware/properties.xml | /bin/sed 's/<property name="vra.gugent.selfmanaged.password.linux" value="//' | /bin/sed 's/"\/>//' | /bin/awk '{print $1}' | /usr/bin/passwd --stdin root && /usr/bin/passwd -e root
+/bin/grep '"vra.gugent.selfmanaged.password.linux"' /usr/share/gugent/site/InstallSoftware/properties.xml | /bin/sed 's/<property name="vra.gugent.selfmanaged.password.linux" value="//' | /bin/sed 's/"\/>//' | /bin/awk '{print $1}' | /usr/bin/passwd --stdin default && /usr/bin/passwd -e default
+/bin/rm -rf /var/tmp/rootpw.sh
