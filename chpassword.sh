4cddcf0fae3f058ef9f199501f07e16e5712bca9
Calvin Robinson <calvintrobinson@gmail.com>
2017-05-10 18:10:36 -0400

    Update chpassword.sh

--- chpassword.sh
+++ chpassword.sh
@@ -4,4 +4,4 @@
 #In this example, the property vra.gugent.selfmanaged.password.linux
 /bin/grep '"vra.gugent.selfmanaged.password.linux"' /usr/share/gugent/site/InstallSoftware/properties.xml | /bin/sed 's/<property name="vra.gugent.selfmanaged.password.linux" value="//' | /bin/sed 's/"\/>//' | /bin/awk '{print $1}' | /usr/bin/passwd --stdin root && /usr/bin/passwd -e root
 /bin/grep '"vra.gugent.selfmanaged.password.linux"' /usr/share/gugent/site/InstallSoftware/properties.xml | /bin/sed 's/<property name="vra.gugent.selfmanaged.password.linux" value="//' | /bin/sed 's/"\/>//' | /bin/awk '{print $1}' | /usr/bin/passwd --stdin default && /usr/bin/passwd -e default
-/bin/rm -rf /var/tmp/rootpw.sh
+/bin/rm -f /var/tmp/rootpw.sh
